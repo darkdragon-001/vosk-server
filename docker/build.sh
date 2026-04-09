bbb6c388b0ef12000f85156b26ebc246e7141319
Dark Dragon <darkdragon-001@web.de>
2026-04-09 18:56:49 +0200

    Fix arm64 build

--- docker/build.sh
+++ docker/build.sh
@@ -3,12 +3,8 @@
 set -e
 set -x
 
-docker build --no-cache --build-arg KALDI_MKL=0 --file Dockerfile.kaldi-vosk-server --tag alphacep/kaldi-vosk-server:latest .
+docker buildx build --platform linux/amd64,linux/arm64 --no-cache --build-arg KALDI_MKL=0 --file Dockerfile.kaldi-vosk-server --tag alphacep/kaldi-vosk-server:latest --push .
 
 for kind in ru en de cn fr es en-in grpc-en en-spk ja hi; do
-    docker build --file Dockerfile.kaldi-${kind} --tag alphacep/kaldi-${kind}:latest .
-done
-
-for kind in vosk-server ru en de cn fr es en-in grpc-en en-spk ja hi; do
-    docker push alphacep/kaldi-${kind}:latest
+    docker buildx build --platform linux/amd64,linux/arm64 --file Dockerfile.kaldi-${kind} --tag alphacep/kaldi-${kind}:latest --push .
 done
